df2c117fd98499387cf043a051a73986be56cc45
Dustin Ward <wardustin90@gmail.com>
2023-02-12 17:36:40 -0700

    first commit

---
+++ hellohuman/src/test/java/com/dustin/hellohuman/HellohumanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dustin.hellohuman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellohumanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
